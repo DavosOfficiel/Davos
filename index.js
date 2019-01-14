4c7e6207d187b08f48e0b09e56aa0067e058fd5c
DavosOfficiel <46665196+DavosOfficiel@users.noreply.github.com>
2019-01-14 08:15:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const cfg require('./index.json'); // a garder en version desktop
-const token = process.env.token // a garder en version heroku
+const cfg require('./index.json');
+const token = process.env.token;
 const prefix = ("?");
 
 bot.on('ready', function () {
@@ -40,5 +40,4 @@ bot.on('message', msg => {
 
 });
 
-bot.login(cfg.token); //a garder en version desktop
-bot.login(token); //a garder en version heroku
+bot.login(token);
