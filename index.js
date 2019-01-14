2229cdc6d7dab974d6ece3f874e72dcc67983c30
DavosOfficiel <46665196+DavosOfficiel@users.noreply.github.com>
2019-01-14 09:35:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,9 +19,6 @@ bot.on('message', msg => {
     if (msg.content === "bonjour"){
         msg.reply("Heureux de te revoir parmis nous.")
     }
-    if (msg.content.match(/salut/i)) {
-            msg.reply('Salut.')
-    }
     if (msg.content === prefix + "site"){
         msg.channel.send("https://alexpgm.000webhostapp.com/")
         console.log("Une personne a demandé pour aller sur ton site.")
