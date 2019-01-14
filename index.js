793d7acc240f5611b735c345fbec029374170c3e
DavosOfficiel <46665196+DavosOfficiel@users.noreply.github.com>
2019-01-14 08:49:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const cfg require('./index.json');
 const token = process.env.token;
 const prefix = ("?");
 bot.on('ready', function () {
@@ -8,36 +7,4 @@ bot.on('ready', function () {
     bot.user.setActivity('rien').catch(console.error)
 });
 
-bot.on('guildMemberAdd', member => {
-    member.createDM().then(channel => {
-        return channel.send('Bienvenue sur le serveur de Alexpgm' + member.displayName)
-        console.log(`${member.displayName} à rejoind le serveur.`)
-    }).catch(console.error)
-});
-
-const ban = require('./kick et ban/ban');
-require('./embed/RichEmbed');
-
-bot.on('message', function (message){
-    if (ban.match(message)){
-        return ban.action(message)
-    }
-});
-
-
-bot.on('message', msg => {
-    if (msg.content === "bonjour"){
-        msg.reply("Heureux de te revoir parmis nous.")
-    }
-    if (msg.content.match(/salut/i)) {
-            msg.reply('Je suis d\'accord avec toi.')
-    }
-    if (msg.content === prefix + "site"){
-        msg.channel.send("https://alexpgm.000webhostapp.com/")
-        console.log("Une personne a demandé pour aller sur ton site.")
-    }
-
-});
-
-bot.login(cfg.token);
 bot.login(token);
