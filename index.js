2201053692c4144fa05c5cae2ecebe6567703d46
DavosOfficiel <46665196+DavosOfficiel@users.noreply.github.com>
2019-01-27 02:42:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,15 +16,18 @@ bot.on('guildMemberAdd', member => {
 });
 
 bot.on('message', msg => {
-    if (msg.content === "*bonjour"){
+    if (msg.content === "!bonjour"){
         msg.reply("Heureux de te revoir parmis nous.")
     }
     if (msg.content === prefix + "site"){
         msg.channel.send("https://alexpgm.000webhostapp.com/")
         console.log("Une personne a demandé pour aller sur ton site.")
     }
-    if (msg.content === "*Salut"){
+    if (msg.content === "!Salut"){
         msg.reply("Comment va tu ?.")
+    }
+    if (msg.content === "!Bien et toi ?"){
+        msg.reply("Oui je vais bien.")
     }
 });
 
