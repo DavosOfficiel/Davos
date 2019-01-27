236bd49b99a3b245d9ce224d57ce5d12e02934fb
DavosOfficiel <46665196+DavosOfficiel@users.noreply.github.com>
2019-01-27 04:04:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = process.env.token;
-const messageEnMinuscules = str.toLowerCase();
 const prefix = ("?");
 
 bot.on('ready', function () {
