745c3d23673a01c1424dc172a838dd8637b17313
DavosOfficiel <46665196+DavosOfficiel@users.noreply.github.com>
2019-01-27 03:53:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,14 +29,6 @@ bot.on('message', msg => {
     if (msg.content === "!Bien et toi ?"){
         msg.reply("Oui je vais bien.")
     }
-    if (commande === "purge"){
-        const deleteCount = parseInt(args[0], 10);
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-        return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-        const fetched = await message.channel.fetchMessages({limit: deleteCount});
-        message.channel.bulkDelete(fetched)
-        .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-    }
 });
 
 bot.login(token);
